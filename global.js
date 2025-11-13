020896d8dc1c98d04a5393887a46231f9471d157
cecilia <caban@ucsd.edu>
2025-11-12 20:26:16 -0800

    initial heatmap + filtering functionality

--- global.js
+++ global.js
@@ -1,12 +1,24 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-async function loadData(day) {
-  let path = `./lib/2024_${String(day).padStart(3, "0")}_LST.json`;
-  const data = await d3.json(path);
-  return data;
-}
-
 let dayToDisplay = 1; // in julian days
+const width = 800;
+const height = 500;
+const margin = { top: 20, right: 20, bottom: 40, left: 60 };
+
+const minx = -140,
+  maxx = -65;
+const miny = 20,
+  maxy = 55;
+
+// axes & scale
+const x = d3
+  .scaleLinear()
+  .domain([minx, maxx])
+  .range([margin.left, width - margin.right]);
+const y = d3
+  .scaleLinear()
+  .domain([miny, maxy])
+  .range([height - margin.bottom, margin.top]);
 
 function convertDate(jDay) {
   // convert julian day to month/day/year format for 2024
@@ -24,9 +36,64 @@ function displayDate(jDay) {
   d3.select("#date").text(`Date: ${dateStr}`);
 }
 
-function renderMap(day) {
-  const data = loadData(day);
-  const svg = d3.select("svg");
+const svg = d3.select("svg");
+
+svg
+  .append("defs")
+  .append("clipPath")
+  .attr("id", "clip")
+  .append("rect")
+  .attr("x", x(minx))
+  .attr("y", y(maxy))
+  .attr("width", x(maxx) - x(minx))
+  .attr("height", y(miny) - y(maxy));
+
+// axes
+const xAxis = d3.axisBottom(x).ticks(10);
+const yAxis = d3.axisLeft(y).ticks(5);
+
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height - margin.bottom})`)
+  .call(xAxis);
+
+svg.append("g").attr("transform", `translate(${margin.left},0)`).call(yAxis);
+
+const mapGroup = svg.append("g").attr("clip-path", "url(#clip)");
+
+// Use a fixed color domain (Kelvin)
+const color = d3.scaleSequential(d3.interpolateYlOrRd).domain([250, 330]);
+
+async function renderMap(day) {
+  // load data
+  let visibleData;
+  let path = `./lib/2024_${String(day).padStart(3, "0")}_LST.json`;
+
+  try {
+    const file = await d3.json(path);
+    const jsonData = file.data;
+    visibleData = jsonData.filter(
+      (d) => d.lon >= minx && d.lon <= maxx && d.lat >= miny && d.lat <= maxy
+    );
+
+    const sampleFraction = 0.6;
+    const subset = visibleData.filter(() => Math.random() < sampleFraction);
+
+    const rectSize = 8;
+    mapGroup.selectAll("rect").remove();
+
+    mapGroup
+      .selectAll("rect")
+      .data(subset)
+      .join("rect")
+      .attr("x", (d) => x(d.lon))
+      .attr("y", (d) => y(d.lat))
+      .attr("width", rectSize)
+      .attr("height", rectSize)
+      .attr("fill", (d) => color(d.LST));
+  } catch (error) {
+    console.error("Error loading JSON:", error);
+  }
 }
 
 function update(newDay) {
